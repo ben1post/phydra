f386005ae31c9718e238b32a23583641e2aa2001
Benjamin Post <whoisbenjaminpost@gmail.com>
2023-07-12 16:53:28 +0200

    commit before restructuring library
    the repo will be restructured from python package to interactive folder structure, focused on Jupyter Notebooks

--- src/phydra/models/sizebased/fluxes/basic.py
+++ src/phydra/models/sizebased/fluxes/basic.py
@@ -17,26 +17,27 @@ class LinearForcingInput:
 @xso.component
 class LinearPhytoMortality:
     """Linear Phytplankton Mortality Flux."""
-    var = xso.variable(dims='phyto', foreign=True, flux='decay', negative=True, description='variable affected by flux')
+    var = xso.variable(dims='phyto', foreign=True, flux='mortality', negative=True, description='variable affected by flux')
     rate = xso.parameter(dims='phyto', description='linear rate of mortality')
 
     @xso.flux(dims='phyto')
-    def decay(self, var, rate):
+    def mortality(self, var, rate):
         """Linear decay function."""
         return var * rate
 
+import numpy as np
 @xso.component
 class QuadraticZooMortality:
     """Quadratic Zooplankton Mortality Flux."""
-    var = xso.variable(foreign=True, dims='zoo', flux='decay', negative=True, description='variable affected by flux')
+    var = xso.variable(foreign=True, dims='zoo', flux='mortality', negative=True, description='variable affected by flux')
     rate = xso.parameter(description='quadratic rate of mortality')
 
     @xso.flux(dims='zoo')
-    def decay(self, var, rate):
+    def mortality(self, var, rate):
         """Flux function describing the quadratic mortality of zooplankton
         according to Banas et al. (2011).
 
         Utilizes the XSO Math module, available at self.m within fluxes,
         to allow for flexible implementation of math functions according
         to solver backend."""
-        return var * self.m.sum(var) * rate
+        return rate * var * np.sum(var)
